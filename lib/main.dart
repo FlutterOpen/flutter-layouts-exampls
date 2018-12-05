896d4d8a8e2fbb7225ad47087363f95069974041
niebin <nbfly01@163.com>
2018-12-05 23:38:28 +0800

    comply the hero screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:layout_demo_flutter/main_page.dart';
 import 'nbmain.dart';
-import 'package:layout_demo_flutter/view/row_column_selector.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter layout',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
       home: Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.white,
         body: NBMain(),
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:layout_demo_flutter/main.dart';
+import "package:flutter_layout/nbmain.dart";
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new NBMain());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
